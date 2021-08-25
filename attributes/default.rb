41c30b95de1f326180a67642c3b8c5ca36b572ff
Robin Douine <r.douine@criteo.com>
2021-08-25 19:28:11 +0200

    Bump to 2.30.0
    Attributes refacto

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,3 @@
-default['gobgp']['binary']['version'] = '2.12.0'
-gobgp_version = default['gobgp']['binary']['version']
-default['gobgp']['binary']['url'] = "https://github.com/osrg/gobgp/releases/download/v#{gobgp_version}/gobgp_#{gobgp_version}_linux_amd64.tar.gz"
-default['gobgp']['binary']['checksum'] = 'e4a3744e0ea6dec5e2ee2338bc6f1fc5cfd321605144c83ed2ad2f6734646996'
-
 default['gobgp']['user'] = 'gobgpd'
 default['gobgp']['group'] = 'gobgpd'
 default['gobgp']['config_file'] = '/etc/gobgpd.conf'
